e5581dcb04ddfe735e7987f2e7d1491665c77e46
潘文波 <john@panwenbo.cn>
2023-04-20 13:37:09 +0800

    添加provider地址

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -263,74 +263,74 @@ export default {
               {
                 label: "ACL4SSR_Online 默认版 分组比较全 (与Github同步)",
                 value:
-                  "https://rules.panwenbo.cn:11443/ACL4SSR/Clash/config/ACL4SSR_Online.ini",
+                  "https://rules.panwenbo.cn:11443/providers/ACL4SSR_Online.ini",
               },
               {
                 label: "ACL4SSR_Online_AdblockPlus 更多去广告 (与Github同步)",
                 value:
-                  "https://rules.panwenbo.cn:11443/ACL4SSR/Clash/config/ACL4SSR_Online_AdblockPlus.ini",
+                  "https://rules.panwenbo.cn:11443/providers/ACL4SSR_Online_AdblockPlus.ini",
               },
               {
                 label: "ACL4SSR_Online_NoAuto 无自动测速 (与Github同步)",
                 value:
-                  "https://rules.panwenbo.cn:11443/ACL4SSR/Clash/config/ACL4SSR_Online_NoAuto.ini",
+                  "https://rules.panwenbo.cn:11443/providers/ACL4SSR_Online_NoAuto.ini",
               },
               {
                 label: "ACL4SSR_Online_NoReject 无广告拦截规则 (与Github同步)",
                 value:
-                  "https://rules.panwenbo.cn:11443/ACL4SSR/Clash/config/ACL4SSR_Online_NoReject.ini",
+                  "https://rules.panwenbo.cn:11443/providers/ACL4SSR_Online_NoReject.ini",
               },
               {
                 label: "ACL4SSR_Online_Mini 精简版 (与Github同步)",
                 value:
-                  "https://rules.panwenbo.cn:11443/ACL4SSR/Clash/config/ACL4SSR_Online_Mini.ini",
+                  "https://rules.panwenbo.cn:11443/providers/ACL4SSR_Online_Mini.ini",
               },
               {
                 label:
                   "ACL4SSR_Online_Mini_AdblockPlus.ini 精简版 更多去广告 (与Github同步)",
                 value:
-                  "https://rules.panwenbo.cn:11443/ACL4SSR/Clash/config/ACL4SSR_Online_Mini_AdblockPlus.ini",
+                  "https://rules.panwenbo.cn:11443/providers/ACL4SSR_Online_Mini_AdblockPlus.ini",
               },
               {
                 label:
                   "ACL4SSR_Online_Mini_NoAuto.ini 精简版 不带自动测速 (与Github同步)",
                 value:
-                  "https://rules.panwenbo.cn:11443/ACL4SSR/Clash/config/ACL4SSR_Online_Mini_NoAuto.ini",
+                  "https://rules.panwenbo.cn:11443/providers/ACL4SSR_Online_Mini_NoAuto.ini",
               },
               {
                 label:
                   "ACL4SSR_Online_Mini_Fallback.ini 精简版 带故障转移 (与Github同步)",
                 value:
-                  "https://rules.panwenbo.cn:11443/ACL4SSR/Clash/config/ACL4SSR_Online_Mini_Fallback.ini",
+                  "https://rules.panwenbo.cn:11443/providers/ACL4SSR_Online_Mini_Fallback.ini",
               },
               {
                 label:
                   "ACL4SSR_Online_Mini_MultiMode.ini 精简版 自动测速、故障转移、负载均衡 (与Github同步)",
                 value:
-                  "https://rules.panwenbo.cn:11443/ACL4SSR/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini",
+                  "https://rules.panwenbo.cn:11443/providers/ACL4SSR_Online_Mini_MultiMode.ini",
               },
               {
                 label: "ACL4SSR_Online_Full 全分组 重度用户使用 (与Github同步)",
                 value:
-                  "https://rules.panwenbo.cn:11443/ACL4SSR/Clash/config/ACL4SSR_Online_Full.ini",
+                  "https://rules.panwenbo.cn:11443/providers/ACL4SSR_Online_Full.ini",
               },
               {
                 label:
                   "ACL4SSR_Online_Full_NoAuto.ini 全分组 无自动测速 重度用户使用 (与Github同步)",
                 value:
-                  "https://rules.panwenbo.cn:11443/ACL4SSR/Clash/config/ACL4SSR_Online_Full_NoAuto.ini",
+                  "https://rules.panwenbo.cn:11443/providers/ACL4SSR_Online_Full_NoAuto.ini",
               },
               {
                 label:
                   "ACL4SSR_Online_Full_AdblockPlus 全分组 重度用户使用 更多去广告 (与Github同步)",
                 value:
-                  "https://rules.panwenbo.cn:11443/ACL4SSR/Clash/config/ACL4SSR_Online_Full_AdblockPlus.ini",
+                  "https://rules.panwenbo.cn:11443/providers/ACL4SSR_Online_Full_AdblockPlus.ini",
               },
               {
                 label:
                   "ACL4SSR_Online_Full_Netflix 全分组 重度用户使用 奈飞全量 (与Github同步)",
                 value:
-                  "https://rules.panwenbo.cn:11443/ACL4SSR/Clash/config/ACL4SSR_Online_Full_Netflix.ini",
+                  "https://rules.panwenbo.cn:11443/providers/ACL4SSR_Online_Full_Netflix.ini",
               },
             ]
           }
